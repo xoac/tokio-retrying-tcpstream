e364c5af304c5d1d5f3d084267d535fe24f4691f
Sylwester Rąpała <sylwesterrapala@outlook.com>
2020-02-02 21:14:52 +0100

    Add description, categories and keywords. Update README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 //! This crate wraps [TcpStream] and [ConnectFuture] into [RetryingTcpStream].
 //!
-//! When you work with connection that are expected to broke you may want add auto reconnect after
+//! When you work with connection that are expected to sometimes broke you may want add auto reconnect after
 //! error is detected. [RetryingTcpStream] makes pollable after returning error.
-//! It mean any time, any method that name start with `poll` return Error - the inner state will
-//! reseted.
+//! It mean any time, any method that name start with `poll` return Error - the inner state will reset.
 //!
 //! When you think about [RetryingTcpStream] you should think about mix [ConnectFuture] and [TcpStream].
 //! When you call `poll_read()` or `poll_write()` on [RetryingTcpStream]
 //! 1. If it is in [ConnectFuture] state -> will go to [TcpStream] state and go to 2.
 //! 2. If it is in [TcpStream] state -> will call requested method retrurning result:
-//!   - Ok(_) -> Normal poll result
-//!   - Err(_) -> Internal state is reset to [ConnectFuture] state. Next poll*() method will try connect.
+//!   - `Ok(_)` -> Normal poll result
+//!   - `Err(_)` -> Internal state is reset to [ConnectFuture] state. Next poll*() method will try connect.
 //!
 //! [RetryingTcpStream] is design to work with [futures-retry]. It's up to you with error are temporary and can be repair by reconnecting.
 //!
@@ -86,7 +85,7 @@ impl RetryingTcpStream {
     }
 }
 
-/// Reimplement methods from [TcpStream]
+/// Reimplement of methods from [TcpStream]
 /// The main differences are:
 /// - `tokio::io::ErrorKind::NotConnected` is returned when inner state is in [ConnectFuture] and
 /// there is no other way to get response.
